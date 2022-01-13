bc1973624cf48673bc581a8c6f1176e230ee18f3
吴钦飞 <wuqinfei_csdn@163.com>
2022-01-13 21:54:49 +0800

    【docs\frontend\03.vue.md】slot

---
+++ codes/vue2/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Panel>
+      内容
+    </Panel>
+
+    <Panel>
+      内容
+
+      <template v-slot:footer>
+        页脚
+      </template>
+    </Panel>
+
+    <Panel>
+      <template v-slot:test="{ list }">
+        <div v-for="item in list" :key="item">{{ item }}</div>
+      </template>
+    </Panel>
+  </div>
+</template>
+
+<script>
+import Panel from './Panel.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Panel
+  }
+}
+</script>
