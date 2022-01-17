5bfe1171b27b8c8c1645096a2c39955987b67401
吴钦飞 <wuqinfei@henrywaltz.cn>
2022-01-17 09:43:57 +0800

    【utils/server.js】添加 /_build 构建URL

--- utils/server.js
+++ utils/server.js
@@ -1,5 +1,6 @@
-const express = require('express');
 const path = require('path');
+const { exec } = require("child_process");
+const express = require('express');
 
 const app = new express();
 
@@ -10,3 +11,30 @@ app.listen(8001, () => {
 const staticPath = path.join(__dirname, '..', 'docs/.vuepress/dist/');
 
 app.use('/', express.static(staticPath));
+
+app.get('/_build', async (req, res) => {
+  await execCommand('git pull');
+  await execCommand('npm run docs:build');
+
+  res.send('build finished!')
+});
+
+function execCommand(command) {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        reject(error);
+        console.log(`[ ${command} ] error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        reject(new Error(stderr));
+        console.log(`[ ${command} ] stderr: ${stderr}`);
+        return;
+      }
+
+      resolve(stdout);
+      console.log(`[ ${command} ] stdout: ${stdout}`);
+    });
+  });
+}
