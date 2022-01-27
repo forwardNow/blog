2d109cdb7c64284850ff5b5eebef682319d31da8
吴钦飞 <wuqinfei_csdn@163.com>
2022-01-27 23:09:35 +0800

    【vue2】02-src-v-model

---
+++ codes/vue2/02-src-v-model/Child.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    我是 Child，我要 <button @click="handleClickButton">改变 parentValue</button>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    value: {
+      type: String, // v-model 默认绑定 value 属性
+    },
+  },
+
+  methods: {
+    handleClickButton() {
+      // v-model 默认通过 input 事件改变父级的值
+      this.$emit('input', '321');
+    }
+  }
+}
+</script>
