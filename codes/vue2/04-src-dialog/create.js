4183373151926f1392849e992ab757b4361d2dcd
吴钦飞 <wuqinfei_csdn@163.com>
2022-02-03 21:52:59 +0800

    【03.vue2.md】5. 弹出层（弹窗）

--- codes/vue2/04-src-dialog/create.js
+++ codes/vue2/04-src-dialog/create.js
@@ -1,11 +1,18 @@
 import Vue from 'vue';
 
+/**
+ * 从头创建指定组件实例，并挂载到 document.body
+ * 
+ * @param component
+ * @param props
+ * @return {Vue|VNode}
+ */
 export default function create(component, props) {
   const vm = new Vue({
     render(createElement) {
       return createElement(component, { props });
     }
-  }).$mount(); // 挂载到“内存 DOM”
+  }).$mount(); // 挂载到“内存 DOM”（生成 $el）
 
   // 将 “内存 DOM” 添加到 DOM 树
   document.body.appendChild(vm.$el);
