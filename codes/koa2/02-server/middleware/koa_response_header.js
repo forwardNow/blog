17ae59ec22f84f8bdb1417f4692faf68e711aa1f
吴钦飞 <wuqinfei_csdn@163.com>
2022-05-17 21:34:39 +0800

    【koa】02.koa2.md： 3.4. 业务逻辑中间件

--- codes/koa2/02-server/middleware/koa_response_header.js
+++ codes/koa2/02-server/middleware/koa_response_header.js
@@ -3,5 +3,8 @@ module.exports = async (ctx, next) => {
 
   ctx.set('content-type', contentType);
 
+  ctx.set('Access-Control-Allow-Origin', '*');
+  ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
   await next();
 };
