a37a13651b4266d60c02ba6db4657169d5e29cff
吴钦飞 <wuqinfei_csdn@163.com>
2022-06-18 20:48:41 +0800

    【EJS】init

---
+++ codes/ejs/01.helloworld.js
@@ -0,0 +1,18 @@
+const ejs = require('ejs');
+
+// 模板
+const template = 'My name is <%= name %>'; 
+
+// 也可以将字符串编译成函数
+// const template = ejs.compile('My name is <%= name %>')
+
+// 数据
+const data = {
+  name: '张三',
+};
+
+// 渲染
+const html = ejs.render(str, data);
+
+console.log(html);
+//=> My name is 张三
