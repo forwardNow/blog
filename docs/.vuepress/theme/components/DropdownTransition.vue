1a86f164421c9c6e94e879c144a9524a3f7e715f
吴钦飞 <wuqinfei_csdn@163.com>
2023-04-03 10:04:25 +0800

    【docs\.vuepress\theme\components】DropdownTransition.vue：去掉菜单折叠动画效果

--- docs/.vuepress/theme/components/DropdownTransition.vue
+++ docs/.vuepress/theme/components/DropdownTransition.vue
@@ -1,10 +1,5 @@
 <template>
-  <transition
-    name="dropdown"
-    @enter="setHeight"
-    @after-enter="unsetHeight"
-    @before-leave="setHeight"
-  >
+  <transition>
     <slot />
   </transition>
 </template>
@@ -14,20 +9,10 @@ export default {
   name: 'DropdownTransition',
 
   methods: {
-    setHeight (items) {
-      // explicitly set height so that it can be transitioned
-      items.style.height = items.scrollHeight + 'px'
-    },
-
-    unsetHeight (items) {
-      items.style.height = ''
-    }
   }
 }
 </script>
 
 <style lang="stylus">
-.dropdown-enter, .dropdown-leave-to
-  height 0 !important
 
 </style>
