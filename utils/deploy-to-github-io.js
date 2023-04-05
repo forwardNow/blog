1395c2142611df56249207ef54d0652b1b47a0a8
吴钦飞 <wuqinfei_csdn@163.com>
2023-04-05 13:16:44 +0800

    【utils】deploy-to-github-io.js：部署到 github.io

---
+++ utils/deploy-to-github-io.js
@@ -0,0 +1,103 @@
+const path = require('path');
+const fs = require('fs');
+const { exec } = require("child_process");
+
+const BLOG_PATH = path.resolve(__dirname, '../');
+const GITHUB_IO_PATH = path.resolve(BLOG_PATH, '../forwardNow.github.io');
+
+const BLOG_DIST_PATH = path.resolve(BLOG_PATH, 'blog/docs/.vuepress/dist');
+
+
+async function run()  {
+  // build: blog
+  // await buildBlog();
+
+  // copy: blog/docs/.vuepress/dist/** -> forwardNow.github.io/
+  copyDistToGithubIo();
+
+  // push: forwardNow.github.io
+  // pushGithubIo();
+}
+
+function buildBlog() {
+  return execCommand('npm run docs:build', { cwd: BLOG_PATH });
+}
+
+function copyDistToGithubIo() {
+  const srcRootPath = BLOG_DIST_PATH;
+  const destRootPath = GITHUB_IO_PATH;
+
+  const copyFile = (filePath) => {
+    const srcFilePath = filePath;
+    const destFilePath = filePath.replace(srcRootPath, destRootPath);
+
+    fs.copyFileSync(srcFilePath, destFilePath);
+  };
+
+  const copyDir = (dir) => {
+    fs.mkdirSync(dir, { recursive: true });
+  };
+
+  const traverse = (dir, callback) => {
+    const filenameList = fs.readdirSync(dir);
+
+    for (let i = 0; i < filenameList.length; i++) {
+      const filename = filenameList[i];
+
+      const isDir = fs.lstatSync(filename).isDirectory();
+      
+      if (isDir) {
+        const subDirPath = path.join(dir, filename);
+        traverse(subDirPath, callback);
+      }
+
+      const filePath = path.join(dir, filename);
+
+      callback({filename, filePath, isDir, parentDir});
+    }
+
+    traverse(srcRootPath, ({ filePath, isDir}) => {
+      if (isDir) {
+        copyDir(filePath);
+        return;
+      }
+
+      copyFile(filePath);
+    });
+  };
+}
+
+async function pushGithubIo() {
+  await execCommand('git pull', { cwd: GITHUB_IO_PATH });
+  await execCommand('git push', { cwd: GITHUB_IO_PATH });
+}
+
+run();
+
+
+//--
+
+function execCommand(command, options) {
+  console.log(`start executing command: ${command}`);
+  return new Promise((resolve, reject) => {
+    exec(
+      command, 
+      (error, stdout, stderr) => {
+        if (error) {
+          reject(error);
+          console.log(`[ ${command} ] error: ${error.message}`);
+          return;
+        }
+        if (stderr) {
+          reject(new Error(stderr));
+          console.log(`[ ${command} ] stderr: ${stderr}`);
+          return;
+        }
+
+        resolve(stdout);
+        console.log(`[ ${command} ] stdout: ${stdout}`);
+      }, 
+      options,
+    );
+  });
+}
