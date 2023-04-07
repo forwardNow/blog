99ea6aa54efe3bf5aa5337f93525e58c1d06d1ad
吴钦飞 <wuqinfei_csdn@163.com>
2023-04-07 21:38:44 +0800

    【docs\front-end\vue2\guide\2.Vue组件化编程.md】单文件组件

---
+++ docs/front-end/vue2/guide/codes/basic/27.单文件组件.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{ name }} - {{ age }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Student',
+  data() {
+    return {
+      name: '张三',
+      age: 18
+    };
+  },
+}
+</script>
+
+<style>
+
+</style>
