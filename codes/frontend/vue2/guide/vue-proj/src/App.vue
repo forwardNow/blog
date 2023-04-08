d511df2ee1acaf1998098e10f372c6218392f604
吴钦飞 <wuqinfei_csdn@163.com>
2023-04-08 23:13:42 +0800

    【codes\frontend\vue2\guide\vue-proj】todo list

--- codes/frontend/vue2/guide/vue-proj/src/App.vue
+++ codes/frontend/vue2/guide/vue-proj/src/App.vue
@@ -1,28 +1,110 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="root">
+    <div class="todo-container">
+      <div class="todo-wrap">
+        <MyHeader :addTodo="addTodo" />
+        <MyList :todos="todos" :toggleTodoDone="toggleTodoDone" :deleteTodo="deleteTodo" />
+        <MyFooter
+          :todos="todos"
+          :setDoneOfAllTodos="setDoneOfAllTodos"
+          :deleteTodosOfDone="deleteTodosOfDone"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import MyHeader from './components/MyHeader.vue';
+import MyList from './components/MyList.vue';
+import MyFooter from './components/MyFooter.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    MyHeader,
+    MyList,
+    MyFooter,
+  },
+  data() {
+    return {
+      todos: [
+        { id: '001', title: '抽烟', done: true },
+        { id: '002', title: '喝酒', done: false },
+        { id: '003', title: '开车', done: true },
+      ],
+    };
+  },
+  methods: {
+    addTodo(todo) {
+      this.todos.unshift(todo);
+    },
+    toggleTodoDone(id) {
+      const targetTodo = this.todos.find((todo) => todo.id === id);
+      targetTodo.done = !targetTodo.done;
+    },
+    deleteTodo(id) {
+      const targetTodoIndex = this.todos.findIndex((todo) => todo.id === id);
+
+      if (targetTodoIndex !== -1) {
+        this.todos.splice(targetTodoIndex, 1);
+      }
+    },
+    setDoneOfAllTodos(done) {
+      this.todos = this.todos.map((todo) => {
+        // eslint-disable-next-line no-param-reassign
+        todo.done = done;
+        return todo;
+      });
+    },
+    deleteTodosOfDone() {
+      this.todos = this.todos.filter((todo) => !todo.done);
+    },
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+/*base*/
+body {
+  background: #fff;
+}
+
+.btn {
+  display: inline-block;
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+}
+
+.btn-danger {
+  color: #fff;
+  background-color: #da4f49;
+  border: 1px solid #bd362f;
+}
+
+.btn-danger:hover {
+  color: #fff;
+  background-color: #bd362f;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
 }
 </style>
