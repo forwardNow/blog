469c5e30d58d3f278a5da76211526de898bff6c3
吴钦飞 <wuqinfei_csdn@163.com>
2023-04-10 22:25:46 +0800

    【codes\frontend\vue2\guide\vue-proj】配置代理

--- codes/frontend/vue2/guide/vue-proj/vue.config.js
+++ codes/frontend/vue2/guide/vue-proj/vue.config.js
@@ -3,4 +3,18 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: 'warning',
+  devServer: {
+    proxy: {
+      '/api1': {
+        target: 'http://localhost:5000',
+        // changeOrigin: true,
+        pathRewrite: { '^/api1': '' },
+      },
+      '/api2': {
+        target: 'http://localhost:5001',
+        changeOrigin: true,
+        pathRewrite: { '^/api2': '' },
+      },
+    },
+  },
 });
