84e45d776f104ec9f25f314d4a117befe13bf572
吴钦飞 <wuqinfei_csdn@163.com>
2023-04-11 18:49:49 +0800

    【docs\front-end\vue2\guide】update

--- codes/frontend/vue2/guide/vue-proj/src/main.js
+++ codes/frontend/vue2/guide/vue-proj/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
+import store from './store';
 
 Vue.config.productionTip = false;
 
@@ -8,4 +9,5 @@ new Vue({
   beforeCreate() {
     Vue.prototype.$bus = this;
   },
+  store,
 }).$mount('#app');
