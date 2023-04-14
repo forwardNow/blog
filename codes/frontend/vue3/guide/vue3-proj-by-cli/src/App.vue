78c4f735929af5bfd100a740ea83637c60b4ca41
吴钦飞 <wuqinfei_csdn@163.com>
2023-04-14 22:41:55 +0800

    【codes\frontend\vue3\guide\vue3-proj-by-cli】update

--- codes/frontend/vue3/guide/vue3-proj-by-cli/src/App.vue
+++ codes/frontend/vue3/guide/vue3-proj-by-cli/src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <p>姓名：{{ name }}</p>
+  <p>年龄：{{ age }}</p>
+  <p><button @click="sayHello">sayHello</button></p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    const name = '张三';
+    const age = 18;
+
+    function sayHello() {
+      alert(`你好，我是${name}，今年${age}岁。`);
+    }
+
+    return {
+      name,
+      age,
+      sayHello,
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
