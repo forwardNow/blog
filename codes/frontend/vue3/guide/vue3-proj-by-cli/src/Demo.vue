2d3d057a0d7b401d987c0d488d0af2cfca829561
吴钦飞 <wuqinfei_csdn@163.com>
2023-04-15 22:33:46 +0800

    【codes\frontend\vue3\guide\vue3-proj-by-cli】update

---
+++ codes/frontend/vue3/guide/vue3-proj-by-cli/src/Demo.vue
@@ -0,0 +1,28 @@
+<template>
+  <p>{{ name }} - {{ age }}</p>
+  <button @click="handleClick">show</button>
+</template>
+
+<script>
+export default {
+  name: 'Demo',
+  props: [ 'name', 'age', /* 'gender' */ ],
+  emits: [ 'show' ],
+  setup(props, context) {
+    // Proxy(Object) {name: '张三', age: 18}
+    console.log('props:', props); 
+
+    // Proxy(Object) {gender: '男'}
+    console.log('context.attrs:', context.attrs);
+
+    // Proxy(Object) {default: ƒ}
+    console.log('context.slots:', context.slots);
+
+    function handleClick() {
+      context.emit('show', 666);
+    }
+
+    return { handleClick };
+  }
+}
+</script>
