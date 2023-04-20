75029f4428b5c88e4d4d78cc4782793d09e0f8b9
吴钦飞 <wuqinfei_csdn@163.com>
2023-04-20 21:36:31 +0800

    【docs\front-end\webpack\guide\01.基础.md】css 优化

--- codes/frontend/webpack/guide/webpack-code/config/webpack.prod.js
+++ codes/frontend/webpack/guide/webpack-code/config/webpack.prod.js
@@ -1,8 +1,28 @@
 const path = require('path');
 const ESLintWebpackPlugin = require('eslint-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 const ROOT_PATH = path.join(__dirname, '../');
+
+const getStyleLoaders = (preProcessor) => {
+  return [
+    MiniCssExtractPlugin.loader,
+    'css-loader',
+    {
+      loader: 'postcss-loader',
+      options: {
+        postcssOptions: {
+          plugins: [
+            'postcss-preset-env',
+          ],
+        },
+      },
+    },
+    preProcessor,
+  ].filter(Boolean);
+};
 
 module.exports = {
   mode: 'production',
@@ -22,22 +42,10 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
       },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.less$/,
-        use: ['style-loader', 'css-loader', 'less-loader'],
-      },
-      {
-        test: /\.s[ac]ss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.styl$/,
-        use: ['style-loader', 'css-loader', 'stylus-loader'],
-      },
+      { test: /\.css$/, use: getStyleLoaders() },
+      { test: /\.less$/, use: getStyleLoaders('less-loader') },
+      { test: /\.s[ac]ss$/, use: getStyleLoaders('sass-loader') },
+      { test: /\.styl$/, use: getStyleLoaders('stylus-loader') },
       {
         test: /\.(png|jpe?g|gif|webp|svg)$/,
         type: 'asset',
@@ -68,5 +76,12 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(ROOT_PATH, './public/index.html'),
     }),
+
+    new MiniCssExtractPlugin({
+      // 指定输出文件名
+      filename: 'static/css/main.css',
+    }),
+
+    new CssMinimizerPlugin(),
   ],
 };
