acf1bed38a747d93c927bb27507821ae5ca4ec5f
吴钦飞 <wuqinfei_csdn@163.com>
2023-05-10 21:07:50 +0800

    【docs\front-end\typescript\guide\01.TypeScript快速入门.md】interface vs type

--- codes/frontend/typescript/guide/01/09.接口.ts
+++ codes/frontend/typescript/guide/01/09.接口.ts
@@ -9,3 +9,17 @@ let person: IPerson = {
     console.log(this.name, 'hi');
   }
 }
+
+type IStudent = {
+  name: string;
+  sayHi(): void;
+}
+
+let student: IStudent = {
+  name: '张三',
+  sayHi() {
+    console.log(this.name, 'hi');
+  }
+}
+
+type StrNum = string | number;
