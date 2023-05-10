8a1c2a6c7fb353df4edff4ea7442426199b04135
吴钦飞 <wuqinfei_csdn@163.com>
2023-05-10 19:02:35 +0800

    【docs\front-end\typescript\guide\01.TypeScript快速入门.md】单独指定

---
+++ codes/frontend/typescript/guide/01/05.函数类型.ts
@@ -0,0 +1,11 @@
+function add(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+const sum = function (num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+const sum2 = (num1: number, num2: number): number  => {
+  return num1 + num2;
+}
