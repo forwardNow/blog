8fd6c2c8955ce6b16fccb709f9d1420790378e88
吴钦飞 <wuqinfei_csdn@163.com>
2023-05-10 19:44:04 +0800

    【docs\front-end\typescript\guide\01.TypeScript快速入门.md】同时指定

--- codes/frontend/typescript/guide/01/05.函数类型.ts
+++ codes/frontend/typescript/guide/01/05.函数类型.ts
@@ -9,3 +9,11 @@ const sum = function (num1: number, num2: number): number {
 const sum2 = (num1: number, num2: number): number  => {
   return num1 + num2;
 }
+
+// 声明时，指定类型
+let getSum: (num1: number, num2: number) => number;
+
+// 赋值
+getSum = (num1, num2) => {
+  return num1 + num2;
+}
