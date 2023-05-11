7b2b9bbfb6e64862ff5ced0d4ef0c64e0641127f
吴钦飞 <wuqinfei_csdn@163.com>
2023-05-11 19:48:21 +0800

    【docs\front-end\typescript\guide\01.TypeScript快速入门.md】原理

---
+++ codes/frontend/typescript/guide/01/18.枚举原理.js
@@ -0,0 +1,8 @@
+var Direction1;
+(function (Direction1) {
+    Direction1["Up"] = "UP";
+    Direction1["Down"] = "DOWN";
+    Direction1["Left"] = "LEFT";
+    Direction1["Right"] = "RIGHT";
+})(Direction1 || (Direction1 = {}));
+var direction = 'UP';
