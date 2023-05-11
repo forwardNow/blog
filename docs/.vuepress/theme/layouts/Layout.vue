7ef5368cf17dd0caecfb1d0720b88109f2f48996
吴钦飞 <wuqinfei_csdn@163.com>
2023-05-11 16:45:57 +0800

    【docs\.vuepress\theme\layouts\Layout.vue】屏蔽右滑显示 sidebar 的功能

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -136,6 +136,8 @@ export default {
     },
 
     onTouchEnd (e) {
+      return; // 屏蔽右滑显示 sidebar 的功能
+
       const dx = e.changedTouches[0].clientX - this.touchStart.x
       const dy = e.changedTouches[0].clientY - this.touchStart.y
       if (Math.abs(dx) > Math.abs(dy) && Math.abs(dx) > 40) {
