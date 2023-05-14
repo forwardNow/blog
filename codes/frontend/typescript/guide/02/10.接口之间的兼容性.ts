e3905801cf489a5238fed2045f65cdb371f79830
吴钦飞 <wuqinfei_csdn@163.com>
2023-05-14 09:36:24 +0800

    Update 10.接口之间的兼容性.ts

--- codes/frontend/typescript/guide/02/10.接口之间的兼容性.ts
+++ codes/frontend/typescript/guide/02/10.接口之间的兼容性.ts
@@ -2,6 +2,8 @@ interface IPoint   { x: number, y: number }
 interface IPoint2D { x: number, y: number }
 interface IPoint3D { x: number, y: number, z: number }
 
+class Point3D { x: number; y: number; z: number }
+
 let p1: IPoint;
 let p2: IPoint2D;
 let p3: IPoint3D;
@@ -11,3 +13,6 @@ p1 = p2;
 
 // 多的赋值给少的
 p1 = p3;
+
+
+p2 = new Point3D();
