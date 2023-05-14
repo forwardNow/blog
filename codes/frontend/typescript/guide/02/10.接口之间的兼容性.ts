5e3eb06dd1e84ad60ba7e65b63765b311d6d6113
吴钦飞 <wuqinfei_csdn@163.com>
2023-05-14 09:33:36 +0800

    Create 10.接口之间的兼容性.ts

---
+++ codes/frontend/typescript/guide/02/10.接口之间的兼容性.ts
@@ -0,0 +1,13 @@
+interface IPoint   { x: number, y: number }
+interface IPoint2D { x: number, y: number }
+interface IPoint3D { x: number, y: number, z: number }
+
+let p1: IPoint;
+let p2: IPoint2D;
+let p3: IPoint3D;
+
+// 相同属性
+p1 = p2;
+
+// 多的赋值给少的
+p1 = p3;
