1b0d6de5cf864f29499db815b946cdb1beefd3c3
吴钦飞 <wuqinfei_csdn@163.com>
2023-05-15 19:20:00 +0800

    Update 11.索引签名类型.ts

--- codes/frontend/typescript/guide/03/11.索引签名类型.ts
+++ codes/frontend/typescript/guide/03/11.索引签名类型.ts
@@ -9,6 +9,15 @@ interface AnyObject {
 let obj: AnyObject = {
   a: 1,
   b: 2,
+};
+
+
+([1, 2]).forEach(() => {});
+
+
+interface MyArray<T> {
+  [index: number]: T;
 }
 
-
+let arr: MyArray<number> = [1, 2]
+console.log( arr[0] );
