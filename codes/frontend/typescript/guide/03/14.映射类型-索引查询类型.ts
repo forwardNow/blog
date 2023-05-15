0908f1670e0fe6c6dfe0690132dff751fef2eac1
吴钦飞 <wuqinfei_csdn@163.com>
2023-05-15 20:56:42 +0800

    Update 14.映射类型-索引查询类型.ts

--- codes/frontend/typescript/guide/03/14.映射类型-索引查询类型.ts
+++ codes/frontend/typescript/guide/03/14.映射类型-索引查询类型.ts
@@ -1,6 +1,16 @@
-type Props = { x: number, y: string }
+type Props = {
+  x: number,
+  y: string,
+  z: boolean,
+  i: number,
+}
 
-// type TypeX = number;
-type TypeX = Props['x'];
+// 获取单个属性的类型
+type Type1 = Props['x']; // number
 
-let x: TypeX = 1;
+// 获取多个属性类型
+type Type2 = Props['x' | 'y']; // number | string
+
+// 获取所有属性的类型
+type Type3 = Props[keyof Props]; // number | string | boolean
+
