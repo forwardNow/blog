41b3b86acc01447657ee3765b4b85aa74952df04
吴钦飞 <wuqinfei_csdn@163.com>
2023-06-17 11:39:47 +0800

    Create 03.appenders-stdout.js

---
+++ codes/backend/nodejs/node/log4js/03.appenders-stdout.js
@@ -0,0 +1,18 @@
+const log4js = require('log4js');
+
+log4js.configure({
+  appenders: {
+    stdout: {
+      type: 'stdout'
+    },
+  },
+  categories: {
+    default: {
+      appenders: ['stdout'],
+      level: 'debug'
+    },
+  },
+});
+
+const logger = log4js.getLogger();
+logger.info('appenders-stdout');
